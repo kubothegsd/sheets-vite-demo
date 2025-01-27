332e19fa088a94a23f00ab08ec28739a480f4ec1
kubothegsd <kubothegsd@gmail.com>
2025-01-27 13:24:35 +1100

    Feed data for demo

--- src/main.ts
+++ src/main.ts
@@ -61,13 +61,127 @@ const { univerAPI } = createUniver({
   ] as any[],
 });
 
+const seedWorkbookData = {
+  "id": "qzhdra",
+  "sheetOrder": [
+      "sgCc4KQhuoLihvN2RuAuK"
+  ],
+  "name": "Test Sheet",
+  "appVersion": "0.5.5",
+  "locale": LocaleType.EN_US,
+  "sheets": {
+      "sgCc4KQhuoLihvN2RuAuK": {
+          "id": "sgCc4KQhuoLihvN2RuAuK",
+          "name": "Sheet1",
+          "tabColor": "",
+          "hidden": 0,
+          "rowCount": 1000,
+          "columnCount": 20,
+          "zoomRatio": 1,
+          "freeze": {
+              "xSplit": 0,
+              "ySplit": 0,
+              "startRow": -1,
+              "startColumn": -1
+          },
+          "scrollTop": 0,
+          "scrollLeft": 0,
+          "defaultColumnWidth": 88,
+          "defaultRowHeight": 24,
+          "mergeData": [],
+          "cellData": {
+              "0": {
+                  "0": {
+                      "v": "Service",
+                      "t": 1,
+                      "s": "VBVvzA"
+                  },
+                  "1": {
+                      "v": "Monthly",
+                      "t": 1,
+                      "s": "5dzgxD"
+                  },
+                  "2": {
+                      "v": "Annual",
+                      "t": 1,
+                      "s": "5dzgxD"
+                  }
+              },
+              "1": {
+                  "0": {
+                      "v": "Netflix ",
+                      "t": 1,
+                      "s": "79CIc3"
+                  },
+                  "1": {
+                      "v": 22.04,
+                      "t": 2,
+                      "s": "FgwM3w"
+                  },
+                  "2": {
+                      "v": 264.48,
+                      "t": 2,
+                      "s": "FgwM3w"
+                  }
+              },
+              "2": {
+                  "0": {
+                      "v": "Prime ",
+                      "t": 1,
+                      "s": "79CIc3"
+                  },
+                  "1": {
+                      "v": 8.66,
+                      "t": 2,
+                      "s": "FgwM3w"
+                  },
+                  "2": {
+                      "v": 103.95,
+                      "t": 2,
+                      "s": "FgwM3w"
+                  }
+              }
+          },
+          "rowData": {
+              "0": {
+                  "h": 21
+              },
+              "1": {
+                  "h": 21
+              },
+              "2": {
+                  "h": 21
+              }
+          },
+          "columnData": {
+              "0": {
+                  "w": 107
+              },
+              "1": {
+                  "w": 100
+              },
+              "2": {
+                  "w": 100
+              }
+          },
+          "showGridlines": 1,
+          "rowHeader": {
+              "width": 46,
+              "hidden": 0
+          },
+          "columnHeader": {
+              "height": 20,
+              "hidden": 0
+          },
+          "rightToLeft": 0
+      }
+  },
+}
 
-univerAPI.createUniverSheet({ name: 'Test Sheet' });
+univerAPI.createUniverSheet(seedWorkbookData);
 
 document.getElementById('test_btn')?.addEventListener('click', () => {
   const activeWorkbook = univerAPI.getActiveWorkbook();
   const snapshot = activeWorkbook?.save();
   console.log('snapshot', snapshot);
-  return 0;
-
 })
